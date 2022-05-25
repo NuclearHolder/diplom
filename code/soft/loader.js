ea6d0efe0454048e95fc333d5d513cd1ca58c3fe
NuclearHolder <nikitos19990212@gmail.com>
2022-05-25 21:37:07 +0300

    add cool fucking function

--- code/soft/loader.js
+++ code/soft/loader.js
@@ -14,7 +14,7 @@ var loader = {
       let arr2 = [];
 
       for (let j = 0; j < size; ++j) {
-        arr2.push((Math.sin(i*0.5)+2)*Math.random());
+        arr2.push(((Math.sin((i/2)*100))*Math.random()*2)*(Math.sin(j/10)*Math.random())*(Math.sin(i*j+10)+10));
       }
 
       arr.push(arr2);
